2a8ad299f5e0ed39958bedd52594dab2f74b0f59
sbalsara05 <balsarasohum@gmail.com>
2024-04-15 09:30:09 -0400

    added styling to Navbar.jsx. Can't get menu to work. Fix soon

---
+++ sbalsara/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+
+import {styles} from '../styles.js'
+import {navLinks} from "../constants";
+import {logo, menu, close} from "../assets/"
+
+const Navbar = () => {
+  const [active, setActive] = useState('');
+    const [toggle, setToggle] = useState(false);
+  return (
+    <nav className={`${styles.paddingX} w-full flex 
+    items-center py-5 fixed z-20 bg-primary`}>
+      <div className="w-full flex justify-between
+      items-center max-w-7xl mx-auto">
+        <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() =>{
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+        >
+         <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer">Sohum Balsara <span className="sm: blockhidden">| Computer Science Student at Ithaca College</span></p>
+        </Link>
+        <ul className="list-nonehidden sm: flex flex-row gap-10">
+            {navLinks.map((Link, index) => (
+                <li key ={Link.id}
+                    className = {`${
+                        active === Link.title
+                            ? "text-white"
+                            : "text-secondary"
+                    } hover:text-white text-[18px] 
+                    font-medium cursor-pointer`}
+                    onClick={() => setActive(Link.title)}
+                >
+                    <a href={`#${Link.id}`}>{Link.title}</a>
+                </li>
+            ))}
+
+        </ul>
+        <div className="sm:hidden flex flex-1
+        justify-end items-center">
+            <img
+                src = {toggle ? close : menu}
+                alt="menu"
+                className="w-[28px] h-[28px] object-contain cursor-pointer"
+                onClick={() => setToggle(!toggle)}
+            />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
