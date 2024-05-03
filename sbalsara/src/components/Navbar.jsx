7c0e9a1897d02634944794ac94b6a0b8b5ee41c1
sbalsara05 <balsarasohum@gmail.com>
2024-05-03 18:25:02 -0400

    Worked on NavBar

--- sbalsara/src/components/Navbar.jsx
+++ sbalsara/src/components/Navbar.jsx
@@ -48,6 +48,27 @@ const Navbar = () => {
                 className="w-[28px] h-[28px] object-contain cursor-pointer"
                 onClick={() => setToggle(!toggle)}
             />
+            <div className={`${!toggle ? "hidden" :
+                "flex"}p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+                <ul className="list-none flex  justify-end items-start flex-col gap-4">
+                    {navLinks.map((Link, index) => (
+                        <li key={Link.id}
+                            className={`${
+                                active === Link.title
+                                    ? "text-white"
+                                    : "text-secondary"
+                            } font-poppins font-medium cursor-pointer text-16px`}
+                            onClick={() => {
+                                setToggle(!toggle)
+                                setActive(Link.title);
+                            }}
+                        >
+                            <a href={`#${Link.id}`}>{Link.title}</a>
+                        </li>
+                    ))}
+
+                </ul>
+            </div>
         </div>
       </div>
     </nav>
