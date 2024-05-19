485fd69e647f5aa943f3a568f80cd745b25701ed
sbalsara05 <balsarasohum@gmail.com>
2024-05-19 14:10:55 -0500

    Added Scroll animation for about section as well as fixed the lighting for the Desktop 3D object. Added optimization for mobile devices

--- sbalsara/src/components/Hero.jsx
+++ sbalsara/src/components/Hero.jsx
@@ -17,13 +17,30 @@ const Hero = () => {
                         className="text-[#915eff]">Sohum</span></h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am currently a student at Ithaca <br
                         className="sm:block hidden"/>
-                        College studying Computer Science and Economics. My experiences include being a research intern
-                        for Professor Douglas Turnbull, and Professor John Barr. Additionally, I am honored to have
-                        earned a spot on the Dean's List for the fall semester.
+                        College studying Computer Science and Economics.
                         I look forward to meeting and coordinating with all of you!</p>
                 </div>
             </div>
             <ComputersCanvas/>
+
+            <div className="absolute xs:bottom-10 bottom-5 w-full flex justify-center items-center">
+                <a href="#about">
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.dev
+                        animate = {{
+                            y: [0,24,0]
+                        }}
+                        transition={{
+                            duration: 1.5,
+                            repeat: Infinity,
+                            repeatType: "spring",
+                        }}
+                        className = "w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+
+                </a>
+            </div>
         </section>
     )
 }
--- sbalsara/src/components/canvas/Computers.jsx
+++ sbalsara/src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
+      <hemisphereLight intensity={4}
                        groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
@@ -23,7 +23,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.5 : 0.6}
-        position={isMobile ? [0, -4, -2.2] : [0, -4, -1.5]}
+        position={isMobile ? [0, -2, -2.2] : [0, -2.7, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -31,6 +31,22 @@ const Computers = ({ isMobile }) => {
 };
 
 const ComputersCanvas = () => {
+    const[isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width:500px)");
+        setIsMobile(mediaQuery.matches);
+
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches)
+        };
+
+        mediaQuery.addEventListener("change",
+            handleMediaQueryChange);
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        }
+    }, []);
 
   return (
     <Canvas
@@ -46,7 +62,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
