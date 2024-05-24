409571e83c1f78c6262b9af422e103567e8139a0
sbalsara05 <balsarasohum@gmail.com>
2024-05-24 12:29:29 -0500

    Tech Section Finished

--- sbalsara/src/constants/index.js
+++ sbalsara/src/constants/index.js
@@ -71,18 +71,18 @@ const technologies = [
         name: "JavaScript",
         icon: javascript,
     },
-    {
-        name: "TypeScript",
-        icon: typescript,
-    },
+    // {
+    //     name: "TypeScript",
+    //     icon: typescript,
+    // },
     {
         name: "React JS",
         icon: reactjs,
     },
-    {
-        name: "Redux Toolkit",
-        icon: redux,
-    },
+    // {
+    //     name: "Redux Toolkit",
+    //     icon: redux,
+    // },
     {
         name: "Tailwind CSS",
         icon: tailwind,
@@ -91,10 +91,10 @@ const technologies = [
         name: "Node JS",
         icon: nodejs,
     },
-    {
-        name: "MongoDB",
-        icon: mongodb,
-    },
+    // {
+    //     name: "MongoDB",
+    //     icon: mongodb,
+    // },
     {
         name: "Three JS",
         icon: threejs,
@@ -107,10 +107,10 @@ const technologies = [
         name: "figma",
         icon: figma,
     },
-    {
-        name: "docker",
-        icon: docker,
-    },
+    // {
+    //     name: "docker",
+    //     icon: docker,
+    // },
 ];
 const experiences = [
     {
