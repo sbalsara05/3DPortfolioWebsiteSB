bf683d47b33c30c6fc9f937a250055306f27277e
sbalsara05 <balsarasohum@gmail.com>
2024-05-29 14:06:08 -0500

    Contact Section Finished

--- sbalsara/src/components/Contact.jsx
+++ sbalsara/src/components/Contact.jsx
@@ -30,7 +30,32 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true)
-    emailjs.send("service_ciffu0s", "template_zueryiu", "gLgeEXSlaKGuOFtJW")
+    emailjs.send(
+        "service_ciffu0s",
+        "template_zueryiu",
+        {
+          from_name: form.name,
+          to_name: "Sohum",
+          from_email: form.email,
+          to_email: "sohum@sohumbalsara.com",
+          message: form.message,
+
+        },
+        "gLgeEXSlaKGuOFtJW"
+        )
+        .then(()=>{
+          setLoading(false);
+          alert("Thank you! I will get back to you as soon as possible!")
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          }, (error) =>{
+            setLoading(false)
+            console.log(error);
+            alert("Oops! Something went wrong!")
+          })
+        })
   }
   return (
       <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden ">
