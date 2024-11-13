1cd84832c14e99119dc36fec88002bf436a8f37f
sbalsara05 <balsarasohum@gmail.com>
2024-11-13 11:36:38 -0500

    Updated Navbar

--- sbalsara/src/components/Navbar.jsx
+++ sbalsara/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-            Sohum Balsara &nbsp; <span className='sm:block hidden'> | Computer Science and Finance Student at
+            Sohum Balsara &nbsp; <span className='sm:block hidden'> | Computer Science and Finance Student (Fintech Concentration) at
             Northeastern University</span>
           </p>
         </Link>
